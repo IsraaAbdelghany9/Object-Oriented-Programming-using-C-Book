de5236ac8893077a7367880a30846df64889aa3c
israa abdelghany <israaabdelghany9@gmail.com>
2024-11-30 01:37:41 +0200

    half of chapter 9 added

--- Chapter_9/CH9-Q1/main.cpp
+++ Chapter_9/CH9-Q1/main.cpp
@@ -6,10 +6,76 @@ class publication
 {
     string title;
     float price ;
+public:
+    void getdata()
+    {
+        cout << "Enter title: ";
+        getline(cin, title);
+        cout << "Enter price: ";
+        cin >> price;
+    }
+
+    void putdata() const
+    {
+        cout << "Title: " << title << endl;
+        cout << "Price: " << price << "$" << endl;
+    }
+
+};
+
+class book : private publication
+{
+    int pageCount;
+public:
+    void getdata()
+    {
+        publication::getdata();
+        cout << "The page count is " ;
+        cin >> pageCount;
+    }
+    void putdata() const
+    {
+        publication::putdata();
+        cout << "The playing time is ";
+        cout << pageCount << endl;
+    }
+};
+
+class tape : private publication
+{
+    float playingTime;
+public:
+    void getdata()
+    {
+        publication::getdata();
+        cout << "Enter the playing time in minutes" ;
+        cin >> playingTime;
+    }
+    void putdata() const
+    {
+        publication::putdata();
+        cout << "The playing time in minutes = " <<playingTime << endl;
+    }
+
+
 };
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    publication pubs;
+    pubs.getdata();
+    pubs.putdata();
+
+    cin.ignore();
+
+    book b ;
+    b.getdata();
+    b.putdata();
+
+    cin.ignore();
+
+    tape t ;
+    t.getdata();
+    t.putdata();
     return 0;
 }
